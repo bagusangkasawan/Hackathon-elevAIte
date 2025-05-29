411e3986d6919c24f2beee76adeb6b829c858e75
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-29 20:45:23 +0700

    fix error

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -4,30 +4,21 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const checkinRoutes = require('./routes/checkinRoutes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./public/swagger.json');
-const serverless = require('serverless-http');
+const setupSwagger = require('./swagger');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
+
+mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/checkin', checkinRoutes);
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
-
 const PORT = process.env.PORT || 3000;
-if (require.main === module) {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
