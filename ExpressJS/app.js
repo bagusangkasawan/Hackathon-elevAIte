a9910f92b0bac2c15abf7a135b942e5ccf29e1cc
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-29 18:36:44 +0700

    fix error

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -20,5 +20,4 @@ mongoose.connect(process.env.MONGO_URI, {})
 app.use('/api/auth', authRoutes);
 app.use('/api/checkin', checkinRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
